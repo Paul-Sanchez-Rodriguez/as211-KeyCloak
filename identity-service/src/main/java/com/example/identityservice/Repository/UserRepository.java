90737950dd42f6ee62a3045a8c19cbe3279a9b3b
Paul-Sanchez-Rodriguez <robert.sanchez@vallegrande.edu.pe>
2023-11-18 01:18:44 -0500

    configuracion Keycloak

--- identity-service/src/main/java/com/example/identityservice/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.identityservice.Repository;
-
-import com.example.identityservice.entity.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository  extends ReactiveCrudRepository<User,Integer> {
-    Mono<User> findByUsernameOrEmail(String username, String email);
-}
